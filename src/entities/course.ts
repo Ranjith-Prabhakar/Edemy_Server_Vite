34d253f5c8e1d803df14d1abcc02eda55e5293ab
Ranjith <ranjithp2294@gmail.com>
2024-02-12 20:26:56 +0530

    course entity file has been created

--- src/entities/course.ts
+++ src/entities/course.ts
@@ -1,8 +1,39 @@
+interface Questions {
+  userId: string;
+  question: string;
+  answers: [
+    {
+      userId: string;
+      answer: string;
+      upvote: number;
+    }
+  ];
+}
+
+interface Module {
+  moduleNo: number;
+  moduleTittle:string,
+  duration: number;
+  questions: Questions;
+}
+
+interface ReviewRating{
+  userId:string;
+  review:string;
+  rating:number
+}
+
+
 export interface ICourse {
   name: string;
+  instructor: string;
+  description:string;
   tags: string[];
   thumbnail: string;
   uplaoadedDate: Date;
   status: "approved" | "pending" | "freez";
   duration: number;
+  modules: Array<Module>;
+  review: Array<ReviewRating>;
+  rating: number
 }
