880251d057de63f7f716de0e3bc1cdc281649934
Ranjith <ranjithp2294@gmail.com>
2024-03-05 21:48:31 +0530

    getting course for landing page has been updated

--- src/frameworks/database/mongoDb/repository/courseRepository/getCoursesForUser.ts
+++ src/frameworks/database/mongoDb/repository/courseRepository/getCoursesForUser.ts
@@ -3,7 +3,7 @@ import courseModel from "../../models/courseModel"
 
 export const getCoursesForUser = async(): Promise<void | ICourseResponse>=>{
   try {
-    const result = await courseModel.find()
+    const result = await courseModel.find({ status :"approved"});
     return {status:200,message:"courses have been fetched",data:result}
   } catch (error:any) {
     throw error
