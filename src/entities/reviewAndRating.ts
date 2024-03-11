4339daa0a28f0325ad33777386fbdb7fc889bfaf
Ranjith <ranjithp2294@gmail.com>
2024-03-11 09:32:06 +0530

    review and rating entity and model has been created

---
+++ src/entities/reviewAndRating.ts
@@ -0,0 +1,12 @@
+export interface IReviewAndRating {
+  _id: string;
+  courseId: string;
+  courseName: string;
+  reviewAndRating: {
+    userId: string;
+    userName: string;
+    date: string;
+    review?: string;
+    rating?: number;
+  }[];
+}
---
+++ src/frameworks/database/models/reviewAndRatingModel.ts
@@ -0,0 +1,51 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { IReviewAndRating } from "../../../entities/reviewAndRating";
+
+const reviewAndRatingSchema: Schema<IReviewAndRating> = new mongoose.Schema(
+  {
+    courseId: {
+      type: String,
+      required: [true, "please provide course Id"],
+      trim: true,
+    },
+    courseName: {
+      type: String,
+      required: [true, "please provide course name"],
+      trim: true,
+    },
+    reviewAndRating: [
+      {
+        userId: {
+          type: String,
+          required: [true, "please provide user id"],
+          trim: true,
+        },
+        userName: {
+          type: String,
+          required: [true, "please provide user name"],
+          trim: true,
+        },
+        date: {
+          type: String,
+          default: new Date().getDate().toLocaleString(),
+        },
+        review: {
+          type: String,
+          trim: true,
+        },
+        rating: {
+          type: Number,
+          trim: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const reviewAndRatingModel: Model<IReviewAndRating> = mongoose.model(
+  "reviewAndRating",
+  reviewAndRatingSchema
+);
+
+export default reviewAndRatingModel;
