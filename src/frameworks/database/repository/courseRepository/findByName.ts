80f150e9c19338b5282463bd1f11433e24dfc4c5
Ranjith <ranjithp2294@gmail.com>
2024-04-17 11:40:31 +0530

    cleanup for framework->repositiory->courseRepository->findByName.ts has been done

--- src/frameworks/database/repository/courseRepository/findByName.ts
+++ src/frameworks/database/repository/courseRepository/findByName.ts
@@ -4,7 +4,6 @@ export const findByName = async (
   courseName: string
 ): Promise<string | void> => {
   try {
-    console.log("reaches findbyname in course repository");
     const result = await courseModel.findOne({ courseName });
     if (result) return "a course already exist in this name";
     return;
