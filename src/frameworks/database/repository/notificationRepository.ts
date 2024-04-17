2b4d31d38398358874fa76ed8a962554483f158e
Ranjith <ranjithp2294@gmail.com>
2024-04-17 11:52:38 +0530

    cleanup for framework->repositiory->notificationRepository.ts has been done

--- src/frameworks/database/repository/notificationRepository.ts
+++ src/frameworks/database/repository/notificationRepository.ts
@@ -10,10 +10,7 @@ export class NotificationRepository implements INotificationRepository {
     value?: ICourse
   ): Promise<boolean | void> {
     try {
-      console.log("key,userid,value", key, userId, value);
       if (value) {
-        console.log("inside value", value);
-
         // if the field to update is new course updates for every user
         const result = await notificationModel.findOneAndUpdate(
           { userId },
@@ -22,13 +19,11 @@ export class NotificationRepository implements INotificationRepository {
         if (result) return true;
         else false;
       } else {
-        console.log("inside no value");
         const result = await notificationModel.findOneAndUpdate(
           { userId },
           { [key]: true },
           { upsert: true, new: true }
         );
-        console.log("inside no value result", result);
         if (result) return true;
         else false;
       }
