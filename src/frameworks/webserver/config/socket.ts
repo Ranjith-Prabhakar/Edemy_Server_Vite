0f4eff1992db6621b8c9f92b6bf5a9b185f85c70
Ranjith <ranjithp2294@gmail.com>
2024-04-17 11:58:04 +0530

    cleanup for framework->webserver->config->socket.ts has been done

--- src/frameworks/webserver/config/socket.ts
+++ src/frameworks/webserver/config/socket.ts
@@ -33,25 +33,3 @@ Io.on("connect", (client) => {
 
 export { httpServer, Io };
 
-// import { Server } from "socket.io";
-// import { app } from "./app";
-// import { createServer } from "http";
-// import { ClientToServerEvents, ServerToClientEvents } from "../../types/socketTypes";
-// require("dotenv").config();
-
-// const httpServer = createServer(app);
-// const Io = new Server<ServerToClientEvents, ClientToServerEvents>(httpServer, {
-//   cors: {
-//     origin: process.env.CLIENT,
-//   },
-// });
-
-// Io.on("connect", (client) => {
-//   console.log("one client has been connected", client.id);
-
-//   client.on("disconnect", () => {
-//     console.log("the client ", client.id, " has been disconected");
-//   });
-// });
-
-// export { httpServer, Io};
