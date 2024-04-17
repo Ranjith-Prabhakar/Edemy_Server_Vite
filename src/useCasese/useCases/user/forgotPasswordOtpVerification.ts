d4a65f8ba7f1ee090bb509a1e0fc518dab6f33d6
Ranjith <ranjithp2294@gmail.com>
2024-04-17 12:41:49 +0530

    cleanup for usecases->usecases->user->forgotPasswordOtpVerification.ts has been done

--- src/useCasese/useCases/user/forgotPasswordOtpVerification.ts
+++ src/useCasese/useCases/user/forgotPasswordOtpVerification.ts
@@ -2,6 +2,7 @@ import { Next, Req } from "../../../frameworks/types/serverPackageTypes";
 import { IOtpRepository } from "../../interface/repository/otpRepository";
 import { IGeneralResponse } from "../../interface/request_And_Response/generalResponse";
 import { IJwt } from "../../interface/services/jwt.types";
+import { catchError } from "../../middlewares/catchError";
 import ErrorHandler from "../../middlewares/errorHandler";
 
 export const forgotPasswordOtpVerification = async (
@@ -13,7 +14,6 @@ export const forgotPasswordOtpVerification = async (
 ): Promise<IGeneralResponse | void> => {
   try {
     const decode = await jwtToken.verifyJwt(token);
-    console.log("forgotPasswordOtpVerification decode", decode);
     if (!decode)
       return next(
         new ErrorHandler(400, "you didn`t made any request for change password")
@@ -25,7 +25,7 @@ export const forgotPasswordOtpVerification = async (
     );
     if (!isExist) return next(new ErrorHandler(400, "entered a wrong OTP..."));
     return { success: true, message: "otp matches " };
-  } catch (error: any) {
-    return next(new ErrorHandler(500, error.message));
+  } catch (error) {
+    catchError(error,next)
   }
 };
