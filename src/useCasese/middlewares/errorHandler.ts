e60a8d47f4b1b6009388b74132a706f9d047bc67
Ranjith <ranjithp2294@gmail.com>
2024-04-17 12:04:43 +0530

    cleanup for usecases->middleware->errorHandler.ts has been done

--- src/useCasese/middlewares/errorHandler.ts
+++ src/useCasese/middlewares/errorHandler.ts
@@ -5,26 +5,7 @@ export class ErrorHandler extends Error {
     this.statusCode = statusCode;
     Error.captureStackTrace(this, this.constructor);
   }
-
-  // static badRequest(msg: string): ErrorHandler {
-  //   return new ErrorHandler(400, msg);
-  // }
-
-  // static unauthorized(msg: string): ErrorHandler {
-  //   return new ErrorHandler(401, msg);
-  // }
-
-  // static forbidden(msg: string): ErrorHandler {
-  //   return new ErrorHandler(403, msg);
-  // }
-
-  // static notFound(msg: string = "Not found"): ErrorHandler {
-  //   return new ErrorHandler(404, msg);
-  // }
-
-  // static internalError(msg: string): ErrorHandler {
-  //   return new ErrorHandler(500, msg);
-  // }
+ 
 }
 
 export default ErrorHandler;
