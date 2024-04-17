49bd1f962ff5ecd96b0eac8809d65d8a26e55f56
Ranjith <ranjithp2294@gmail.com>
2024-04-17 13:17:04 +0530

    cleanup for usecases->usecases->user->userSession.ts has been done

--- src/useCasese/useCases/user/userSession.ts
+++ src/useCasese/useCases/user/userSession.ts
@@ -1,11 +1,11 @@
 import { IUser } from "../../../entities/user"
 import { Next, Req } from "../../../frameworks/types/serverPackageTypes"
-import ErrorHandler from "../../middlewares/errorHandler"
+import { catchError } from "../../middlewares/catchError"
 
 export const userSession = async(req:Req,next:Next):Promise<IUser | void>=>{
 try {
   return await req.user as IUser
-} catch (error:any) {
-  return next(new ErrorHandler(500,error.message))
+} catch (error) {
+catchError(error,next)
 }
 }
