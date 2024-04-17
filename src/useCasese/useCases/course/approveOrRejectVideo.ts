f4d5f8d622a34cc5b407711ce233cd0adff7d2e2
Ranjith <ranjithp2294@gmail.com>
2024-04-17 12:24:30 +0530

    cleanup for usecases->usecases->course->approveOrRejectVideos.ts has been done

--- src/useCasese/useCases/course/approveOrRejectVideo.ts
+++ src/useCasese/useCases/course/approveOrRejectVideo.ts
@@ -1,14 +1,14 @@
 import { Next, Req } from "../../../frameworks/types/serverPackageTypes";
 import { ICourseRepository } from "../../interface/repository/courseRepository";
 import { ICourseResponse } from "../../interface/request_And_Response/course";
-import ErrorHandler from "../../middlewares/errorHandler";
+import { catchError } from "../../middlewares/catchError";
 
 export const approveOrRejectVideo =async(courseRepository:ICourseRepository,req:Req,next:Next): Promise<void | ICourseResponse>=>{
   try {
     const {courseId,action} = req.body
     return await courseRepository.approveOrRejectVideo(courseId,action)
-  } catch (error:any) {
-    return next(new ErrorHandler(500,error.message))
+  } catch (error) {
+    catchError(error,next)
     
   }
 } 
