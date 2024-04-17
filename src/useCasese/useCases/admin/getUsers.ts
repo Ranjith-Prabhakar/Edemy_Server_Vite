163004cdd5e0ba4fa6d4d1f0df626a566a132461
Ranjith <ranjithp2294@gmail.com>
2024-04-17 12:17:24 +0530

    cleanup for usecases->usecases->admin->getUsers.ts has been done

--- src/useCasese/useCases/admin/getUsers.ts
+++ src/useCasese/useCases/admin/getUsers.ts
@@ -1,11 +1,12 @@
 import { Next } from "../../../frameworks/types/serverPackageTypes";
 import { IUserRepository } from "../../interface/repository/userRepository";
-import ErrorHandler from "../../middlewares/errorHandler";
+import { catchError } from "../../middlewares/catchError";
 
 export const getUsers = async (userRepository: IUserRepository, next: Next) => {
   try {
     return await userRepository.getUsers("user");
-  } catch (error: any) {
-    return next(new ErrorHandler(500, error.message));
+  } catch (error) {
+    catchError(error,next)
+    // return next(new ErrorHandler(500, error.message));
   }
 };
