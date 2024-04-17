e4a1b27215801b6d7a2305f6693a92b636d21379
Ranjith <ranjithp2294@gmail.com>
2024-04-17 11:41:33 +0530

    cleanup for framework->repositiory->courseRepository->getCourseByCategory.ts has been done

--- src/frameworks/database/repository/courseRepository/getCourseByCategory.ts
+++ src/frameworks/database/repository/courseRepository/getCourseByCategory.ts
@@ -9,17 +9,8 @@ export const getCourseByCategory = async (
   filter: string
 ): Promise<void | ICourseCategoryBaseResponse> => {
   try {
-    console.log(
-      "category pageNumber frequency sort filter @@@@@@@@@@ [filter]",
-      category,
-      pageNumber,
-      frequency,
-      sort,
-      filter,
-      [filter]
-    );
+   
     const sortValue = sort === "A-Z" ? -1 : 1;
-    console.log("sortValue ######", sortValue);
     if (category === "all category") {
       const length = (await courseModel.find()).length;
 
@@ -81,70 +72,3 @@ export const getCourseByCategory = async (
   }
 };
 
-// import { ICourseCategoryBaseResponse } from "../../../../useCasese/interface/request_And_Response/course";
-// import courseModel from "../../models/courseModel";
-
-// export const getCourseByCategory = async (
-//   category: string,
-//   pageNumber: number,
-//   frequency: number,
-//   sort: string,
-//   filter: string
-// ): Promise<void | ICourseCategoryBaseResponse> => {
-//   try {
-//     console.log(
-//       "category pageNumber frequency sort filter @@@@@@@@@@",
-//       category,
-//       pageNumber,
-//       frequency,
-//       sort,
-//       filter
-//     );
-//     const sortValue = sort === "A-Z" ? -1 : 1
-//     if (category === "all category") {
-//       const length = (await courseModel.find()).length;
-
-//       if (length) {
-//         const end = pageNumber * frequency;
-//         const start = end - frequency;
-//         const result = await courseModel.find().skip(start).limit(end);
-//         return {
-//           status: 200,
-//           message: "couses have been fetched successfully",
-//           hasMore: length - end > 0,
-//           data: result,
-//         };
-//       } else {
-//         return {
-//           hasMore: false,
-//           message: "no courses for this category",
-//           status: 400,
-//         };
-//       }
-//     } else {
-//       const length = (await courseModel.find({ category })).length;
-//       if (length) {
-//         const end = pageNumber * frequency;
-//         const start = end - frequency;
-//         const result = await courseModel
-//           .find({ category })
-//           .skip(start)
-//           .limit(end);
-//         return {
-//           status: 200,
-//           message: "couses have been fetched successfully",
-//           hasMore: length - end > 0,
-//           data: result,
-//         };
-//       } else {
-//         return {
-//           hasMore: false,
-//           message: "no courses for this category",
-//           status: 400,
-//         };
-//       }
-//     }
-//   } catch (error) {
-//     throw error;
-//   }
-// };
