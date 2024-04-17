909a9a141a1cbd8b16c0aff1abfb53a87769a8ad
Ranjith <ranjithp2294@gmail.com>
2024-04-17 11:47:03 +0530

    cleanup for framework->repositiory->courseRepository->updatePurchase.ts has been done

--- src/frameworks/database/repository/courseRepository/updatePurchas.ts
+++ src/frameworks/database/repository/courseRepository/updatePurchas.ts
@@ -4,16 +4,11 @@ export const updatePurchas = async (
   courseId: string
 ): Promise<boolean | void> => {
   try {
-    console.log(
-      "inside updatePurchas repo enging courseId",
-      courseId,
-      typeof courseId
-    );
+    
     const result = await courseModel.findOneAndUpdate(
       { _id: courseId },
       { $inc: { noOfPurchase: 1 } }
     );
-    console.log("inside updatePurchas repo enging result", result);
 
     return !!result;
   } catch (error) {
