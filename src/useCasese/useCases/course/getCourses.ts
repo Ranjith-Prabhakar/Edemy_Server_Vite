9e3d6f52fe0d49d5fe04f122cbea0cb240a5e924
Ranjith <ranjithp2294@gmail.com>
2024-04-17 12:27:47 +0530

    cleanup for usecases->usecases->course->getCourses.ts has been done

--- src/useCasese/useCases/course/getCourses.ts
+++ src/useCasese/useCases/course/getCourses.ts
@@ -1,7 +1,7 @@
 import { Next, Req } from "../../../frameworks/types/serverPackageTypes";
 import { ICourseRepository } from "../../interface/repository/courseRepository";
 import { ICourseResponse } from "../../interface/request_And_Response/course";
-import ErrorHandler from "../../middlewares/errorHandler";
+import { catchError } from "../../middlewares/catchError";
 
 export const getCourses = async (
   courseRepository: ICourseRepository,
@@ -9,7 +9,7 @@ export const getCourses = async (
 ): Promise<void | ICourseResponse> => {
   try {
     return await courseRepository.getCourses()
-  } catch (error: any) {
-    return next(new ErrorHandler(500, error.message));
+  } catch (error) {
+    catchError(error,next)
   }
 };
