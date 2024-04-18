19100e29df3fb8bb9cc5519117d17fefa4dc7d28
Ranjith <ranjithp2294@gmail.com>
2024-04-18 09:57:30 +0530

    src/controller/middleware/tokenOption.ts has been updated by changing the samesit = strict from none

--- src/controllers/middleware/tokenOptions.ts
+++ src/controllers/middleware/tokenOptions.ts
@@ -42,7 +42,7 @@ export const accessTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000), //5 hour
   maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "none",
+  sameSite: "strict",
   domain: '.digi-world.online',
   path: '/',
   secure: accessTokenProductionMode,
@@ -51,7 +51,7 @@ export const refreshTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000), // 3 days
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "none",
+  sameSite: "strict",
   domain: '.digi-world.online',
   path: '/',
   secure: accessTokenProductionMode,
